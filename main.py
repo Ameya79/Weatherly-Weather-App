be1d92aeacd54b2e5c02f929303680cf0784225b
Ameya79 <acclaptop47@gmail.com>
2025-06-29 04:32:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,21 +58,10 @@ if city.strip() != "":
 
 
 st.markdown("---")
-st.markdown("""
-<b>Built by Ameya Kulkarni</b><br>
-
-<a href="https://www.linkedin.com/in/ameya-kulkarni-a31b74246" target="_blank">
-    <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="30"/>
-</a>
-
-&nbsp;
-
-<a href="https://github.com/Ameya79" target="_blank">
-    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="30"/>
-</a>
-""", unsafe_allow_html=True)
-
-
+st.markdown(
+    "🔧 *Built by [Ameya Kulkarni](https://www.linkedin.com/in/ameya-kulkarni-a31b74246) • "
+    "[GitHub](https://github.com/Ameya79)*"
+)
 
 # as per weather api documentation the data of weather is stored in such way :
 
